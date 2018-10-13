270013c261a8690575555ee6d1b0b498e6c048d7
Emily Parr <eparr@student.neumont.edu>
2018-10-13 12:45:36 -0600

    Main Added

--- MutiThreadServer/src/main/java/server/Main.java
+++ MutiThreadServer/src/main/java/server/Main.java
@@ -1,3 +1,12 @@
 package server;
+
+import java.io.IOException;
+
 public class Main {
+
+	public static void main(String[] args) throws IOException {
+		HttpServer server = new HttpServer(1234);
+		server.run();
+
+	}
 }
