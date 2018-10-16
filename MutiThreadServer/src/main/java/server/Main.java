b74177d42a2a6bff7f9e77557400b989bfe48de2
Emily Parr <eparr@student.neumont.edu>
2018-10-16 10:54:54 -0600

    Socket Thread Created and Working

--- MutiThreadServer/src/main/java/server/Main.java
+++ MutiThreadServer/src/main/java/server/Main.java
@@ -3,6 +3,8 @@ package server;
 import java.io.IOException;
 
 public class Main {
+	
+	public static final int REQUEST_COUNT = 10; 
 
 	public static void main(String[] args) throws IOException {
 		HttpServer server = new HttpServer(1234);
